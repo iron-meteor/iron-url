eb59d8e531a12c1c36360e6cefa8f5003fcbb68c
Chris Mather <mather.chris@gmail.com>
2014-07-01 16:17:36 -0700

    Add package and license.

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: "iron-path",
+  summary: "Compile paths into regular expressions.",
+  version: "0.1.0",
+  githubUrl: "https://github.com/eventedmind/iron-path"
+});
+
+Package.on_use(function (api) {
+  api.add_files('lib/compile.js');
+  api.add_files('lib/path.js');
+  api.export('Iron');
+});
+
+Package.on_test(function (api) {
+  api.use('iron-path');
+  api.use('tinytest');
+  api.use('test-helpers');
+  api.add_files('test/path_test.js', ['client', 'server']);
+});
