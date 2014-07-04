d6d80459880aad61a1cdfc88cb55339cd9a57f66
Chris Mather <mather.chris@gmail.com>
2014-07-04 14:40:00 -0700

    change name from Path to Url

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: "iron-path",
-  summary: "Compile paths into regular expressions.",
+  name: "iron-url",
+  summary: "Url utilities and support for compiling a url into a regular expression.",
   version: "0.1.0",
-  githubUrl: "https://github.com/eventedmind/iron-path"
+  githubUrl: "https://github.com/eventedmind/iron-url"
 });
 
 Package.on_use(function (api) {
   api.add_files('lib/compiler.js');
-  api.add_files('lib/path.js');
+  api.add_files('lib/url.js');
   api.export('Iron');
 });
 
 Package.on_test(function (api) {
-  api.use('iron-path');
+  api.use('iron-url');
   api.use('tinytest');
   api.use('test-helpers');
-  api.add_files('test/path_test.js', ['client', 'server']);
+  api.add_files('test/url_test.js', ['client', 'server']);
 });
