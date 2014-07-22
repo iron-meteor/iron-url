7272b37b3a7042035c6d6a582e15c8a834b57daf
Tom Coleman <tom@thesnail.org>
2014-07-22 18:00:18 +1000

    Updated package.js

--- package.js
+++ package.js
@@ -1,21 +1,20 @@
 Package.describe({
-  name: "iron-url",
   summary: "Url utilities and support for compiling a url into a regular expression.",
   version: "0.1.0",
-  githubUrl: "https://github.com/eventedmind/iron-url"
+  git: "https://github.com/eventedmind/iron-url"
 });
 
 Package.on_use(function (api) {
   api.use('underscore');
-  api.use('iron-core');
-  api.imply('iron-core');
-  api.add_files('lib/compiler.js');
-  api.add_files('lib/url.js');
+  api.use('iron:core');
+  api.imply('iron:core');
+  api.addFiles('lib/compiler.js');
+  api.addFiles('lib/url.js');
 });
 
 Package.on_test(function (api) {
-  api.use('iron-url');
+  api.use('iron:url');
   api.use('tinytest');
   api.use('test-helpers');
-  api.add_files('test/url_test.js', ['client', 'server']);
+  api.addFiles('test/url_test.js', ['client', 'server']);
 });
