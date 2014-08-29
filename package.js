a2ce82ffe144162f8f1d942cd1e8f430c7635e30
Chris Mather <mather.chris@gmail.com>
2014-08-29 13:26:14 -0700

    fix up package.js for 0.9.1-rc2

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Url utilities and support for compiling a url into a regular expression.",
-  version: "0.1.0",
+  version: "0.4.0-rc0",
   git: "https://github.com/eventedmind/iron-url"
 });
 
 Package.on_use(function (api) {
-  api.use('underscore');
-  api.use('iron:core');
+  api.use('underscore@1.0.0');
+  api.use('iron:core@0.3.2');
   api.imply('iron:core');
   api.add_files('lib/compiler.js');
   api.add_files('lib/url.js');
