4724debccbd5268873b90fefe1c11d55a279fd87
Chris Mather <mather.chris@gmail.com>
2014-12-18 19:11:01 -0800

    bump version to 1.0.6

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Url utilities and support for compiling a url into a regular expression.",
-  version: "1.0.4",
+  version: "1.0.6",
   git: "https://github.com/eventedmind/iron-url"
 });
 
 Package.on_use(function (api) {
   api.use('underscore@1.0.0');
 
-  api.use('iron:core@1.0.4');
+  api.use('iron:core@1.0.6');
   api.imply('iron:core');
 
   api.add_files('lib/compiler.js');
